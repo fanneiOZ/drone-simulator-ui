967133cac10d546ef08a47c16cd95539281c00fe
Panya Batphantana <fanneioz@yahoo.co.th>
2020-05-17 00:42:22 +0700

    NG 9 local environment and configuration setup
    NG dependencies, cat and TS config
    
    Some setting
    
    
    Boiler 🌡🍲
    
    
    Prerequisites for NG
    
    Angular config for stackblitz
    
    A bit more ...
    
    Setup and configuration for local serve

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, Input, Inject } from '@angular/core'
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app',
+    templateUrl: './template/app.template.html',
 })
 export class AppComponent {
-  title = 'app';
+    name: string = 'Just test'
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,12 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-
-import { AppComponent } from './app.component';
+import { BrowserModule } from '@angular/platform-browser'
+import { NgModule } from '@angular/core'
+import { AppComponent } from './app.component'
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    imports: [BrowserModule],
+    exports: [AppComponent],
+    declarations: [AppComponent],
+    providers: [AppComponent],
+    bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
